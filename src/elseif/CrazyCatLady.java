29f97bcbe07e7465f6e6e8d0095f59d2d2c08a50
Carnecchiad <league@WTS21.attlocal.net>
2017-11-12 16:07:29 -0800

    Crazy Cat Lady, Shiny Objects, High Low game

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,19 +5,32 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String x = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int b = Integer.parseInt(x);
 		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+if (b>3) {
+	JOptionPane.showMessageDialog(null, "You are a crazy cat lady!");
+}
+if (b<4 && b>0) {
+	playVideo("https://www.youtube.com/watch?v=FHH6hIc2GyE");
+}
+if (b==0) {
+	playVideo("https://www.youtube.com/watch?v=rO-4F1_czBU");
+}
+}
+
 
 		// 4. If they have 3 or less, call the method below to show them a cat video
 
 		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
 		
-	}
+	
 
 	static void playVideo(String videoURL) {
 		try {
--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,20 +5,40 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(1000000)+1;
 		// 2. Print out the random variable above
 		
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
+		for (int i = 0; i < 21; i++) {
+			
+		
+String x = JOptionPane.showInputDialog("Guess a number between 1 and 100.");
 
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+int b = Integer.parseInt(x);
 			// 5. if the guess is correct
+if (b==random) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	break;
+}
+if (b>random) {
+	JOptionPane.showMessageDialog(null, "Too high");
+}
+if (b<random) {
+	JOptionPane.showMessageDialog(null, "Too low");
+}
+}
+}
+}
+
 				// 6. win
 			// 7. if the guess is high
 				// 8. tell them it's too high
@@ -26,8 +46,8 @@ public class HighLowGame {
 				// 10. tell them it's too low
 
 		// 12. tell them they lose
-	}
+	
 
-}
+
 
 
